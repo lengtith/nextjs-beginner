2d014cdfb48170d959dce5fa135be671aaa8e593
lengtith <sotith1998@gmail.com>
2026-03-15 11:37:15 +0700

    landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
-"use client";
+import React from "react";
 
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
 }
