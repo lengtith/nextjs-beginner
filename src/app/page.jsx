4061b730ad1439179b039fd37d7cd02efb2e96c2
lengtith <sotith1998@gmail.com>
2026-03-22 08:50:16 +0700

    implement on landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import { FeatureSection } from "../components/FeatureSection";
+import { Footer } from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <FeatureSection />
+      <Footer />
     </div>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
           necessitatibus repudiandae
         </p>
       </div>
-      <div>
+      <div className="flex items-center gap-4">
         <Button size={"lg"} className="mt-1">
           Started
           <ArrowRight />
